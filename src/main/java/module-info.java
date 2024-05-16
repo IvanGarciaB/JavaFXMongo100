bf3779b1e2630c9cdddce0e333ae5ed1aeadf393
Ivan <ivan@campusfp.es>
2024-05-16 14:40:29 +0200

    visualizar datos

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.empresa.javafxmongo100 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires mongo.java.driver;
 
 
     opens com.empresa.javafxmongo100 to javafx.fxml;
